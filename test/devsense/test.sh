ca8819ed5c88a5b6ac92aa8e40197f066cb66df0
kreemer <kevin@familie-studer.ch>
2026-01-23 16:13:32 +0100

    fix: tests for devsense recognition

--- test/devsense/test.sh
+++ test/devsense/test.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "version" devsense --version
+check "binary" which devsense-php-ls
 
 # Report result
 reportResults
